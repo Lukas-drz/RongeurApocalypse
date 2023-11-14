1659f6b594383dd828a5d286272989721e0f4335
Lukas-drz <47811332+Lukas-drz@users.noreply.github.com>
2023-11-14 16:45:17 +0100

    Update Créature.js

--- Créature.js
+++ Créature.js
@@ -1,11 +1,12 @@
-
 class Creature {
-  constructor(reproductionRate, perception, strength) {
+  constructor(reproductionRate, perception, strength, gender) {
+    // vérification des valeurs
     if (reproductionRate < 1 || perception < 1 || strength < 1) {
       throw new Error('Les valeurs doivent être supérieures ou égales à 1');
     }
     this.reproductionRate = reproductionRate;
     this.perception = perception;
     this.strength = strength;
+    this.gender = gender; // Ajout d'un attribut "genre"
   }
 }
